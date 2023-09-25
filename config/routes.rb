ec8da3b6a15393ce8d8285021ed85da389efd3de
EC2 Default User <132760017+kyo2929@users.noreply.github.com>
2023-09-25 03:17:58 +0000

    [Create]controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'new/index'
-  get 'new/show'
-  get 'new/edit'
+
+  root to: 'homes#top'
+  resources :books, only:[:index, :show, :edit, :create, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
