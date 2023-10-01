e374ab0178ba27af1aa49e67766748084500893a
EC2 Default User <132760017+kyo2929@users.noreply.github.com>
2023-10-01 07:40:36 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
     @book = Book.new
     @books = Book.all
   end
-  
+
   def create
     @book = Book.new(book_params)
     if @book.save
@@ -14,7 +14,7 @@ class BooksController < ApplicationController
       @books = Book.all
     end
   end
-  
+
   def show
     @book = Book.find(params[:id])
   end
@@ -22,12 +22,21 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      redirect_to book_path(@book.id)
+    end
+  end
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
 
